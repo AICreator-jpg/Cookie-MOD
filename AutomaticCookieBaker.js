a3b64bcc4450add1d4601a9c188c85d9556f22dd
AICreator-jpg <kousei1121.t@gmail.com>
2026-08-31 18:20:43 +0900

    Update AutomaticCookieBaker.js

--- AutomaticCookieBaker.js
+++ AutomaticCookieBaker.js
@@ -192,7 +192,7 @@ var mod = {
       }
     }, 500);
     
-    Game.Notify("Automation MOD", "ver 10.0", "", 1);
+    Game.Notify("Automation MOD", "ver 14.0", "", 1);
   },
   save: function() {},
   load: function() {}
