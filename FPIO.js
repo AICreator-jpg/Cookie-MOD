771f7b3f303e8f5aebb6e43a5d2e2fa133e6753d
AICreator-jpg <kousei1121.t@gmail.com>
2026-09-03 18:44:43 +0900

    Update FPIO.js

--- FPIO.js
+++ FPIO.js
@@ -31,18 +31,29 @@ Game.registerMod("fthof_planner_internal", {
             let width = 256;
             let chunks = 6;
             let key = [];
+            let mix = [];
             let s = [];
             
             for (let i = 0; i < width; i++) {
+                key[i] = 0;
+                mix[i] = 0;
                 s[i] = i;
             }
+
             for (let i = 0; i < seedStr.length; i++) {
-                key[i] = seedStr.charCodeAt(i);
+                key[i & (width - 1)] = seedStr.charCodeAt(i);
+            }
+
+            let mix_val = 0;
+            for (let i = 0; i < seedStr.length; i++) {
+                mix_val = (mix_val * 19) + seedStr.charCodeAt(i);
+                mix_val = mix_val | 0;
+                mix[i & (width - 1)] = mix_val & (width - 1);
             }
 
             let j = 0;
             for (let i = 0; i < width; i++) {
-                j = (j + s[i] + (key[i % key.length] || 0)) & (width - 1);
+                j = (j + s[i] + key[i] + mix[i]) & (width - 1);
                 let t = s[i]; s[i] = s[j]; s[j] = t;
             }
 
@@ -93,7 +104,7 @@ Game.registerMod("fthof_planner_internal", {
 
                 let html = `
                     <div style="text-align: center; margin-bottom: 10px;">
-                        <h3 style="color: #ecc45e; font-size: 18px; margin: 0;">FtHoF プランナー (v75.0.0)</h3>
+                        <h3 style="color: #ecc45e; font-size: 18px; margin: 0;">FtHoF プランナー (v77.0.0)</h3>
                         <p style="font-size: 11px; color: #ccc; margin: 5px 0;">アセンド固定シード: <b style="color:#ecc45e; font-family:monospace; font-size:13px;">${trueSeed}</b> | 現在の総詠唱回数: <b style="color:#fff; font-size:14px;">${spellsCount}</b> 回</p>
                     </div>
                     <table style="width: 100%; border-collapse: collapse; font-size: 11px; text-align: left;">
