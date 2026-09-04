d1055df674f9d8cc58aeea9dd9388cc1c564bba0
AICreator-jpg <kousei1121.t@gmail.com>
2026-09-04 17:24:16 +0900

    Update FPIO.js

--- FPIO.js
+++ FPIO.js
@@ -34,9 +34,90 @@ Game.registerMod("fthof_planner_internal", {
             }, 10);
         }
 
+        function createTrueFtHoFMathRandom(seedStr) {
+            var Math_obj = Math;
+            var width_val = 256;
+            var chunks_val = 6;
+            var digits_val = 52;
+            
+            function k_func(a) {
+                var b, c = a.length, e = this, f = 0, g = (e.i = e.j = 0), h = (e.S = []);
+                for (c || (a = [c++]); width_val > f; ) h[f] = f++;
+                for (f = 0; width_val > f; f++) {
+                    h[f] = h[(j = width_val - 1 & (j + h[f] + a[f % c] + (b[f % b.length] || 0)))];
+                    h[j] = b;
+                }
+                (e.g = function(a) {
+                    for (var b, c = 0, f = e.i, g = e.j, h = e.S; a--; ) {
+                        f = width_val - 1 & (f + 1);
+                        g = width_val - 1 & (g + h[f]);
+                        b = h[f]; h[f] = h[g]; h[g] = b;
+                        c = c * width_val + h[width_val - 1 & (h[f] + h[g])];
+                    }
+                    return (e.i = f), (e.j = g), c;
+                })(width_val);
+            }
+
+            function l_func(a, b) {
+                var e, c = [], d = (typeof a)[0];
+                if (b && 'o' == d) {
+                    for (e in a) {
+                        try {
+                            c.push(l_func(a[e], b - 1));
+                        } catch (f) {}
+                    }
+                }
+                return c.length ? c : 's' == d ? a : a + '\0';
+            }
+
+            function m_func(a, b) {
+                for (var d, c = a + '', e = 0; c.length > e; ) {
+                    b[width_val - 1 & e] = width_val - 1 & ((d ^= 19 * b[width_val - 1 & e]) + c.charCodeAt(e++));
+                }
+                return o_func(b);
+            }
+
+            function n_func(c) {
+                try {
+                    return (a.crypto.getRandomValues((c = new Uint8Array(d))), o_func(c));
+                } catch (e) {
+                    return [+new Date(), a, a.navigator.plugins, a.screen, o_func(b)];
+                }
+            }
+
+            function o_func(a) {
+                return String.fromCharCode.apply(0, a);
+            }
+
+            var g_val = Math_obj.pow(width_val, chunks_val),
+                h_val = Math_obj.pow(2, digits_val),
+                i_val = 2 * h_val,
+                j_val = chunks_val - 1;
+
+            var j_arr = [],
+                p_val = m_func(l_func([seedStr], 3), j_arr),
+                q_obj = new k_func(j_arr);
+
+            var prng_func = function() {
+                for (var a = q_obj.g(chunks_val), b = g_val, c = 0; h_val > a; ) {
+                    a = (a + q_obj.g(1)) * width_val;
+                    b *= width_val;
+                    c = q_obj.g(1);
+                }
+                for (; a >= i_val; ) {
+                    a /= 2;
+                    b /= 2;
+                    c >>>= 1;
+                }
+                return (a + c) / b;
+            };
+
+            return prng_func;
+        }
+
         function calculateFtHoFPlannerData() {
             let currentTower = Game.Objects['Wizard tower'];
-            if (!currentTower || !currentTower.minigame || !Math.seedrandom) return;
+            if (!currentTower || !currentTower.minigame) return;
             
             let M = currentTower.minigame;
             let spellsCount = M.spellsCastTotal;
@@ -47,7 +128,7 @@ Game.registerMod("fthof_planner_internal", {
 
             let html = `
                 <div style="text-align: center; margin-bottom: 10px;">
-                    <h3 style="color: #ecc45e; font-size: 18px; margin: 0;">FtHoF プランナー (v112.0.0)</h3>
+                    <h3 style="color: #ecc45e; font-size: 18px; margin: 0;">FtHoF プランナー (v120.0.0)</h3>
                     <p style="font-size: 11px; color: #ccc; margin: 5px 0;">アセンド固定シード: <b style="color:#ecc45e; font-family:monospace; font-size:13px;">${trueSeed}</b> | 現在の総詠唱回数: <b style="color:#fff; font-size:14px;">${spellsCount}</b> 回</p>
                 </div>
                 <table style="width: 100%; border-collapse: collapse; font-size: 11px; text-align: left;">
@@ -70,18 +151,19 @@ Game.registerMod("fthof_planner_internal", {
                 let futureCast = spellsCount + i;
                 let targetSeedStr = trueSeed + '/' + futureCast;
 
-                let allSpells = new Array(10);
-                Math.seedrandom(targetSeedStr);
-                for (let i1 = 0; i1 < 10; i1++) {
-                    allSpells[i1] = Math.random();
-                }
+                let normalRng = createTrueFtHoFMathRandom(targetSeedStr);
+                let seasonRng = createTrueFtHoFMathRandom(targetSeedStr);
+                let normalFailRng = createTrueFtHoFMathRandom(targetSeedStr);
+                let seasonFailRng = createTrueFtHoFMathRandom(targetSeedStr);
 
-                let rawSeedValue = allSpells[1];
+                let localRngForSeed = createTrueFtHoFMathRandom(targetSeedStr);
+                localRngForSeed();
+                let rawSeedValue = localRngForSeed();
 
-                let normalSuccess = predictRawFtHoF(0, 0, allSpells);
-                let seasonSuccess = predictRawFtHoF(0, 1, allSpells);
-                let normalFail = predictRawFtHoF(1, 0, allSpells);
-                let seasonFail = predictRawFtHoF(1, 1, allSpells);
+                let normalSuccess = predictRawFtHoF(0, 0, normalRng);
+                let seasonSuccess = predictRawFtHoF(0, 1, seasonRng);
+                let normalFail = predictRawFtHoF(1, 0, normalFailRng);
+                let seasonFail = predictRawFtHoF(1, 1, seasonFailRng);
                 
                 let failCondition = getRawFailCondition(rawSeedValue);
 
@@ -129,22 +211,21 @@ Game.registerMod("fthof_planner_internal", {
             div.innerHTML = Game.fthof_planner_html_cache;
             menu.appendChild(div);
         }
-        function predictRawFtHoF(backfire, isSeasonMod, allSpells) {
-            let ptr = 0;
-            ptr++; 
+        function predictRawFtHoF(backfire, isSeasonMod, localRng) {
+            localRng();
             let choice = '';
             if (!backfire) {
-                if (allSpells[ptr++] < 0.15) {
+                if (localRng() < 0.15) {
                     choice = 'blab';
                 } else {
-                    let r = allSpells[ptr++];
+                    let r = localRng();
                     let clickFrenzyChance = 0.15;
                     let bldgSpecChance = 0.1;
                     let stormChance = 0.1;
                     let lumpChance = 0.01;
                     if (r < clickFrenzyChance) {
                         choice = 'click frenzy';
-                        if (allSpells[ptr++] < 0.05) choice = 'blood frenzy';
+                        if (localRng() < 0.05) choice = 'blood frenzy';
                     } else if (r < clickFrenzyChance + bldgSpecChance) {
                         choice = 'building special';
                     } else if (r < clickFrenzyChance + bldgSpecChance + stormChance) {
@@ -154,24 +235,24 @@ Game.registerMod("fthof_planner_internal", {
                     } else {
                         let list = ['frenzy', 'multiply cookies'];
                         if (isSeasonMod) list.push('season_placeholder_cookie');
-                        choice = list[Math.floor(allSpells[ptr++] * list.length)];
+                        choice = list[Math.floor(localRng() * list.length)];
                         if (choice === 'season_placeholder_cookie') {
                             choice = 'frenzy'; 
                         }
                     }
                 }
             } else {
-                if (allSpells[ptr++] < 0.1) {
+                if (localRng() < 0.1) {
                     choice = 'blab';
                 } else {
-                    let r = allSpells[ptr++];
+                    let r = localRng();
                     let bloodFrenzyChance = 0.1;
                     let cursedFingerChance = 0.1;
                     let stormChance = 0.1;
                     let lumpChance = 0.003;
                     if (r < bloodFrenzyChance) {
                         choice = 'blood frenzy';
-                        if (allSpells[ptr++] < 0.05) choice = 'click frenzy';
+                        if (localRng() < 0.05) choice = 'click frenzy';
                     } else if (r < bloodFrenzyChance + cursedFingerChance) {
                         choice = 'cursed finger';
                     } else if (r < bloodFrenzyChance + cursedFingerChance + stormChance) {
@@ -181,7 +262,7 @@ Game.registerMod("fthof_planner_internal", {
                     } else {
                         let list = ['clot', 'ruins'];
                         if (isSeasonMod) list.push('season_placeholder_cookie');
-                        choice = list[Math.floor(allSpells[ptr++] * list.length)];
+                        choice = list[Math.floor(localRng() * list.length)];
                         if (choice === 'season_placeholder_cookie') {
                             choice = 'clot'; 
                         }
